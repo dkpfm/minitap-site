a49efc027c96555bcd9bfb0bf4bad3301b2ac573
Xavier (Jack) <jackjackbach@gmail.com>
2024-10-03 09:28:27 +0200

    evert "ph timer"
    This reverts commit e4e3841bb76b533f3f7fa6fff6695b828971fee2.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,5 @@
 <template>
     <div class="layout">
-        <ClientOnly>
-            <a
-                class="ph"
-                href="https://www.producthunt.com/posts/minitap"
-                target="_blank"
-            >
-                <img src="/assets/images/ph.svg" />
-                <div class="time">
-                    <div class="hours">{{ hoursRemain }}h</div>
-                    <div class="minutes">
-                        {{ minutesRemain }}:{{ secondsRemain }}
-                    </div>
-                </div>
-            </a>
-        </ClientOnly>
         <LayoutHeader />
         <main>
             <slot />
@@ -43,28 +28,6 @@ useSeoMeta({
     ogImage: "https://minitap.io/og-image.jpg",
     twitterCard: "summary_large_image",
 });
-
-import moment from "moment";
-import "moment-timezone";
-import { useRafFn } from "@vueuse/core";
-
-const date = "2024-10-03T09:00:00";
-
-// let yearsRemain = dur.years();
-// let monthsRemain = dur.months();
-// let daysRemain = dur.days();
-const hoursRemain = ref("");
-const minutesRemain = ref("");
-const secondsRemain = ref("");
-
-useRafFn(() => {
-    const dur = moment.duration(
-        moment(date).tz("Europe/Madrid").diff(moment()),
-    );
-    hoursRemain.value = dur.hours();
-    minutesRemain.value = dur.minutes();
-    secondsRemain.value = dur.seconds();
-});
 </script>
 <style lang="scss" scoped>
 main {
@@ -74,30 +37,5 @@ main {
     min-height: 100%;
     max-width: 100%;
     overflow-x: hidden;
-}
-.ph {
-    text-decoration: none;
-    color: white;
-    position: fixed;
-    z-index: 99;
-    right: 20px;
-    top: 20px;
-    filter: drop-shadow(0px 2px 7px rgba(0, 0, 0, 0.3));
-    .time {
-        position: absolute;
-        bottom: 83px;
-        width: 100%;
-        text-decoration: none;
-        color: white;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        font-weight: bold;
-        line-height: 1em;
-        font-size: 13px;
-        .minutes {
-            font-size: 9px;
-        }
-    }
 }
 </style>
